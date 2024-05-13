8570cb27db51384f276783dd803537194c1b73cb
doehna <dorota.wojdecka@gmail.com>
2024-05-13 10:31:53 +0200

    dropdown icon fix

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,10 +1,12 @@
+import * as constants from "./constants"
+
 export function displayDropdownOptions() {
     const options = document.querySelector(".options-box");
     options.style.display = options.style.display === "inline" ? "none" : "inline";
 }
 
 export function changeDropdownDisplay(fontName, fontFamily) {
-    let select = document.querySelector(".select");
+    let select = document.querySelector(".select").firstElementChild;
     select.innerHTML = fontName;
     select.style.fontFamily = fontFamily;
 }
